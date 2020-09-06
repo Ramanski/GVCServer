cbe144e7d22b70ca35400bafbc4f01fc31298b03
Роман Смирнов <3romanski@gmail.com>
2020-09-06 14:20:44 +0300

    Removed SA submodule

--- Data/TrainProfile.cs
+++ Data/TrainProfile.cs
@@ -12,6 +12,7 @@ namespace GVCServer.Data
             this.CreateMap<Train, TrainModel>()
                 .ForMember(ts => ts.Index, m => m.MapFrom(t => string.Format($"{t.FormStation.Substring(0,4)} {t.Ordinal.ToString().PadLeft(3,'0')} {t.DestinationStation.Substring(0,4)}")))
                 .ForMember(ts => ts.DateOper, m => m.MapFrom(t => t.FormTime))
+                .ForMember(tm => tm.TrainKind, m => m.MapFrom(t => t.TrainKindId))
                 .ForMember(ts => ts.LastOperation, m => m.MapFrom(t => t.OpTrain.Select(o => o.KopNavigation.Mnemonic).FirstOrDefault().Trim()));
 
             this.CreateMap<OpVag, VagonModel>()
@@ -23,7 +24,8 @@ namespace GVCServer.Data
 
             this.CreateMap<TrainModel, Train>()
                 .ForMember(t => t.Ordinal, m => m.MapFrom(tl => short.Parse(tl.Index.Substring(5, 3))))
-                .ForMember(t => t.FormTime, m => m.MapFrom(tm => tm.DateOper));
+                .ForMember(t => t.FormTime, m => m.MapFrom(tm => tm.DateOper))
+                .ForMember(t => t.TrainKindId, m => m.MapFrom(tm => tm.TrainKind));
         }
     }
 }
